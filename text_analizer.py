75c39cd507c6380b209a13f5c853c62710d80755
Jacobo J. Pedrosa <jacoboj.pedrosa@Jacobos-Laptop.local>
2025-12-11 12:11:53 +0100

    creada funcion word_list

--- text_analizer.py
+++ text_analizer.py
@@ -2,12 +2,17 @@
 print("Text Analizer")
 
 
-def word_list(t):
-    pass
+def word_list(text):
+    """Esta funcion divide el texto en una lista de palabras manteniendo el orden"""
+    return text.split(' ')
 
 
 if __name__ == "__main__":
-    print("Test Text Analizer")
+    print("==Test Text Analizer==")
+    # print(len(word_list("Hola que tal")))
+
+    assert len(word_list("Hola que tal")) == 3, "word_list failed and not returned the required quantity of words..."
+    assert word_list("Hola que tal") == ['Hola','que','tal'], "word_list failed and not returned the required list of words..."
 
 
-    print(word_list("Hola que tal"))
+    print("Todo OK")
